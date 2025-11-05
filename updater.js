9377e9f16c0cbd924d94861f9cb4f6ccc091a975
꧁Voltage꧂ <f2v74yt@gmail.com>
2025-11-04 20:12:23 -0600

    Update updater.js

--- updater.js
+++ updater.js
@@ -2,7 +2,7 @@
   window.FoxyUI_UpdateMeta = {
     version: 16.1, // increment this when you push a new version
     releaseDate: "2025-11-04",
-    updateURL: "https://raw.githubusercontent.com/<youruser>/<yourrepo>/main/foxyui-v16.js",
+    updateURL: "https://raw.githubusercontent.com/Zirmith/FoxyUI-JS-UI-LIB/refs/heads/main/foxyui-v16.js",
     changelog: [
       "Improved performance and styling.",
       "Added new Three.js helper features.",
