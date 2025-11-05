91cb0c72c18436e3a608c3ae11b549170fada619
꧁Voltage꧂ <f2v74yt@gmail.com>
2025-11-04 20:07:17 -0600

    Create updater.js

---
+++ updater.js
@@ -0,0 +1,12 @@
+(() => {
+  window.FoxyUI_UpdateMeta = {
+    version: 16.1, // increment this when you push a new version
+    releaseDate: "2025-11-04",
+    updateURL: "https://raw.githubusercontent.com/<youruser>/<yourrepo>/main/foxyui-v16.js",
+    changelog: [
+      "Improved performance and styling.",
+      "Added new Three.js helper features.",
+      "Minor bug fixes."
+    ]
+  };
+})();
